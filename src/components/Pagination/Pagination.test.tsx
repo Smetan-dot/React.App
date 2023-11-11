49cd69b3d8d8755580b79fa98c7797e5be20fac9
Smetan-dot <smetanaandrei@gmail.com>
2023-11-11 13:30:33 +0300

    feat: add test for Pagination

---
+++ src/components/Pagination/Pagination.test.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+import { it, expect, describe } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import Pagination from './Pagination';
+
+describe('check Pagination', async () => {
+  it('check render 4 buttons', () => {
+    render(
+      <BrowserRouter>
+        <Pagination />
+      </BrowserRouter>
+    );
+    expect(screen.getAllByRole('button')).toHaveLength(4);
+    expect(screen.getAllByRole('combobox')).toBeDefined();
+    cleanup();
+  });
+});
